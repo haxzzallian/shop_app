14c2befd9487ac1dfa9f7e65b80bd407933bb178
haxzzallian <temitope.owolabi@outlook.com>
2023-11-28 07:29:47 +0100

    DDivider(),

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products.dart';
 import './screens/cart_screen.dart';
+import './screens/orders_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -37,6 +38,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailscreen.routeName: (ctx) => ProductDetailscreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
         },
         //routes: ,
       ),
